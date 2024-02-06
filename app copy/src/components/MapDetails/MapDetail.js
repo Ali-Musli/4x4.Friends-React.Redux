705db617841b9d2d9513472330d5d65d3fd7885f
Ali <alimusli7777@gmail.com>
2024-02-06 20:07:43 +0200

    details component edited

--- app copy/src/components/MapDetails/MapDetail.js
+++ app copy/src/components/MapDetails/MapDetail.js
@@ -4,23 +4,61 @@ import { useReduxAction } from "../../hooks/useReduxAction";
 import { useReduxState } from "../../hooks/useReduxState";
 import { mapsSlice } from "../../reducers/maps";
 
+import style from "./MapDetail.module.css"
+
 function MapsDetails() {
     let { mapId } = useParams();
-    
+
     const fetchMapById = useReduxAction(mapsSlice.actions.fetchMapById);
     const currentMap = useReduxState((state) => state.maps.mapById);
-    
+
     useEffect(() => {
         fetchMapById(mapId)
     }, [mapId]);
 
 
-    return(
-        <section>
-            <div>
-                <img src={currentMap.imgUrl}></img>
+    return (
+        <div className={style["main"]}>
+            <div className={style["main-content"]}>
+                <img className={style["content-img"]} src={currentMap.imgUrl}></img>
+                <div className={style["content-info"]}>
+                    <div className={style["content-info-total"]}>
+                        <div>
+                            <div className={style["content-info-total-item"]} >
+                                <i className="fa-solid fa-map-location-dot"></i>
+                                <p>Тип на терена: {currentMap.terrainType}</p>
+                            </div>
+                            <div className={style["content-info-total-item"]} >
+                                <i className="fa-solid fa-arrow-up-short-wide"></i>
+                                <p>Ниво на трудност:  {currentMap.hardLevel} / 10</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div className={style["content-info-total-item"]} >
+                                <i className="fa-solid fa-location-dot"></i>
+                                <p>Регион: {currentMap.region}</p>
+                            </div>
+                            <div className={style["content-info-total-item"]} >
+                                <i className="fa-solid fa-flag-checkered"></i>
+                                <p>Дистанция: {currentMap.distance} km.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <h3 className={style["content-info-titleDescription"]}>Описание на картата</h3>
+                    <p className={style["content-info-description"]}>{currentMap.description}</p>
+                </div>
             </div>
-        </section>
+            <div className={style["main-comments"]}>
+                <div>
+                    <input placeholder="Добваи своя коментар тук..." />
+                    <button>Добави</button>
+                    <p>Иван: картата е супер!</p>
+                    <p>Пешо: Топ!</p>
+                    <p>Иван: Бива!</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
