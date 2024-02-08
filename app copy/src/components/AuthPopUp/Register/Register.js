0c5c1643de01eb64f1269cea9ddc3ae1265f5376
Ali <alimusli7777@gmail.com>
2024-02-08 21:42:40 +0200

    edit register component

--- app copy/src/components/AuthPopUp/Register/Register.js
+++ app copy/src/components/AuthPopUp/Register/Register.js
@@ -19,34 +19,56 @@ const Register = ({ isRegisterShow }) => {
                 <div className={style["login-popup-inner"]}>
                     <i className="fa-solid fa-user"></i>
                     <h2 className={style["login-popup-title"]} >Register</h2>
-                    <input
-                        type="text"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Confirm Password"
-                        value={confirmPass}
-                        onChange={(e) => setConfirmPass(e.target.value)}
-                    />
+                    <form>
+                        <div>
+                            <label className={style["label"]} htmlFor='username'>Username</label>
+                            <input
+                                name='username'
+                                type="text"
+                                placeholder="Username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+
+                        <div>
+                            <label className={style["label"]} htmlFor='email'>Email</label>
+                            <input
+                                name='email'
+                                type="text"
+                                placeholder="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+
+                        <div>
+                            <label className={style["label"]} htmlFor='password'>Password</label>
+                            <input
+                                name='password'
+                                type="password"
+                                placeholder="********"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+
+                        <div>
+                            <label className={style["label"]} htmlFor='rePassword'>Repeat Password</label>
+                            <input
+                                name='rePassword'
+                                type="text"
+                                placeholder="********"
+                                value={confirmPass}
+                                onChange={(e) => setConfirmPass(e.target.value)}
+                            />
+                        </div>
+                    </form>
                     <div className={style["div-btn"]}>
                         <button className={style["cls-btn"]} onClick={isRegisterShow}>Close</button>
                         <button className={style["auth-btn"]} onClick={handleRegister}>Register</button>
                     </div>
+                    <p className={style["account-exist"]}>You already have account? <a>Login</a></p>
                 </div>
             </div>
         </>
